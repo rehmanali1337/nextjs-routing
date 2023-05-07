2a837bdcc62b932151c2d05360da35c3f1c523c7
Rehman Ali <rehmanali.9442289@gmail.com>
2023-05-07 16:47:29 +0500

    Add more details on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-  <div>Home Page</div>
+  <div>Home Page
+    Developed by: Rehman Ali
+    Telegram: @rehmanali1337
+    Available Routes
+    <ul>
+      <li>/blog</li>
+      <li>/dashboard</li>
+      <li>/analytics</li>
+      <li>/settings</li>
+      <li>/settings/profile</li>
+      <li>/settings/password</li>
+    </ul>
+  </div>
   )
 }
